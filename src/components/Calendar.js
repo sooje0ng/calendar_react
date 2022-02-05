a48286bb8f023f8b0146d81f974300f84a432bab
soojeong <tnwjd7890@naver.com>
2022-02-05 22:56:34 +0900

    calendar_second

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,36 +1,82 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Calendar.scss";
+import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
+import API from "../config/apiSettings.json";
 
 function Calendar() {
     const d = new Date();
 
     const arDay = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
     const arLastDate = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    const [firstDay, setFirstDay] = useState(
-        d.getDay() - (d.getDate() % 7) + 1
-    );
     const [arDate, setArDate] = useState([]);
+    const [selected, setSelected] = useState({
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        date: d.getDate(),
+    });
 
     useEffect(() => {
-        makeCal();
+        makeCal(selected);
+        getMonthlySchedules();
+        getDailySchedules();
     }, []);
 
-    const makeCal = () => {
+    const makeCal = (data) => {
+        const firstDay = new Date(`${data.year}-${data.month}-01`).getDay();
         const newArDate = [];
         for (let i = 0; i < firstDay; i++) {
             newArDate.push("");
         }
-        for (let i = 0; i < arLastDate[d.getMonth()]; i++) {
+        for (let i = 0; i < arLastDate[data.month - 1]; i++) {
             newArDate.push(i + 1 + "");
         }
         setArDate(newArDate);
     };
 
+    const prevMonth = () => {
+        const newMonth = {
+            year: selected.month === 1 ? selected.year - 1 : selected.year,
+            month: selected.month === 1 ? 12 : selected.month - 1,
+            date: -1,
+        };
+
+        setSelected(newMonth);
+        makeCal(newMonth);
+    };
+
+    const getMonthlySchedules = async () => {
+        const result = await (
+            await fetch(API.url + "/calendar?year=2022&month=2")
+        ).json();
+        console.log(result);
+    };
+    const getDailySchedules = async () => {
+        const result = await (
+            await fetch(API.url + "/calendar?year=2022&month=2&date=5")
+        ).json();
+        console.log(result);
+    };
+
+    const nextMonth = () => {
+        const newMonth = {
+            year: selected.month === 12 ? selected.year + 1 : selected.year,
+            month: selected.month === 12 ? 1 : selected.month + 1,
+            date: -1,
+        };
+
+        setSelected(newMonth);
+        makeCal(newMonth);
+    };
+
     return (
         <div className="Calendar">
             <header>
-                {/*시멘틱태그 */}
-                <p>My Calendar</p>
+                <AiFillCaretLeft size={30} color="white" onClick={prevMonth} />
+                <div>
+                    <p className="year">{selected.year}</p>
+                    <p className="month">{selected.month}</p>
+                </div>
+                <AiFillCaretRight size={30} color="white" onClick={nextMonth} />
             </header>
             <div className="content">
                 <ul className="arDay">
@@ -45,9 +91,7 @@ function Calendar() {
                         <li key={index} className="date">
                             <span
                                 className={
-                                    index === d.getDate() - 1 + firstDay
-                                        ? "today"
-                                        : ""
+                                    value == selected.date ? "today" : ""
                                 }
                             >
                                 {value}
